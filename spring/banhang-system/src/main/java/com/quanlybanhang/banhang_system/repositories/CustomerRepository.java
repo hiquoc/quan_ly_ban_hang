37e0bf03606c37f2f5b01c2c8f59ced216c89174
hiquoc <130445689+hiquoc@users.noreply.github.com>
2025-10-05 22:27:14 +0700

    Delete spring/banhang-system directory

--- spring/banhang-system/src/main/java/com/quanlybanhang/banhang_system/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quanlybanhang.banhang_system.repositories;
-
-import com.quanlybanhang.banhang_system.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
--- spring/banhang-system/src/main/java/com/quanlybanhang/banhang_system/repositories/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quanlybanhang.banhang_system.repositories;
-
-import com.quanlybanhang.banhang_system.models.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-}
