43f296e193627a2250b72790ce2c2457f5958d79
Huy Dang <dangquochuy26122003@gmail.com>
2025-10-25 16:52:45 +0700

    25/10

--- spring/Auth_Service/src/main/java/com/doan/auth_service/AuthServiceApplication.java
+++ spring/Auth_Service/src/main/java/com/doan/auth_service/AuthServiceApplication.java
@@ -1,6 +1,5 @@
 package com.doan.auth_service;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients
 public class AuthServiceApplication {
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 }
