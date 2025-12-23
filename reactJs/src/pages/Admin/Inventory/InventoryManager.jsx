15f159fc95d60fef5c79846841901da0518c933a
hiquoc <dangquochuy26122003@gmail.com>
2025-12-23 12:16:24 +0700

    show export btn

--- reactJs/src/pages/Admin/Inventory/InventoryManager.jsx
+++ reactJs/src/pages/Admin/Inventory/InventoryManager.jsx
@@ -252,12 +252,12 @@ export default function InventoryManager() {
       <div className="flex flex-col md:flex-row justify-between items-center mb-6 gap-4">
         <h2 className="text-2xl font-semibold text-gray-800">Quản lý tồn kho</h2>
         <div className="flex gap-2">
-          {/*<button
+          <button
             onClick={() => setShowExportModal(true)}
             className="px-3 py-2 bg-green-600 text-white rounded hover:bg-green-500 transition"
           >
             Xuất file
-          </button>*/}
+          </button>
           <button
             onClick={() => { loadData(0) }}
             className="flex items-center px-4 py-2 border border-gray-700 text-gray-800 rounded hover:bg-gray-200 transition"
